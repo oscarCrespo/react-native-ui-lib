c458985bb036b2d09cd05993c3a3d7af63fad3c4
oscar.cornejo <oscar.cornejo@apiture.com>
2018-03-23 14:55:39 -0400

    Added condition for safe testing

--- src/helpers/Constants.js
+++ src/helpers/Constants.js
@@ -1,10 +1,10 @@
-import {Platform, Dimensions, NativeModules} from 'react-native';
+import { Platform, Dimensions, NativeModules } from "react-native";
 
-const {StatusBarManager} = NativeModules;
-const {height, width} = Dimensions.get('window');
+const { StatusBarManager } = NativeModules;
+const { height, width } = Dimensions.get("window");
 
-export const isAndroid = Platform.OS === 'android';
-export const isIOS = Platform.OS === 'ios';
+export const isAndroid = Platform.OS === "android";
+export const isIOS = Platform.OS === "ios";
 export const screenWidth = width;
 export const screenHeight = height;
 export const isSmallScreen = isIOS ? screenWidth <= 320 : screenWidth <= 360;
@@ -13,6 +13,6 @@ export let statusBarHeight = isIOS ? 20 : StatusBarManager.HEIGHT; // eslint-dis
 export const isIphoneX = isIOS && !Platform.isPad && !Platform.isTVOS && (screenHeight === 812 || screenWidth === 812);
 
 // override guesstimate height with the actual height from StatusBarManager
-if (isIOS) {
+if (isIOS && StatusBarManager.getHeight) {
   StatusBarManager.getHeight(data => (statusBarHeight = data.height));
 }
